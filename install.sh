6a6b32cf68a692db707cb13deef55ad71e951369
FII14 <121723975+FII14@users.noreply.github.com>
2023-02-26 22:12:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# File to install z2j
 
-cp src/z2j /usr/bin
-chmod +x /usr/bin/z2j
+read -p "Do you want to install z2j [Y/n]: " ask
+
+if [[ "${ask}" == "y" || "${ask}" == "Y" ]]; then
+  cp src/z2j /usr/bin
+  chmod +x /usr/bin/z2j
+  echo "z2j installed successfully."
+  exit
+
+elif [[ "${ask}" == "n" || "${ask}" == "N" ]]; then
+  exit
+
+else
+  echo "Wrong input."
+  exit
+fi  
